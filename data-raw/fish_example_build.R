536f9290d62e2245712ab88da374db975685adfd
Eli Felts <elifelts@gmail.com>
2025-12-30 13:19:58 -0700

    changed what gets exported for example detections

--- data-raw/fish_example_build.R
+++ data-raw/fish_example_build.R
@@ -18,4 +18,4 @@ detection.sum <- detections_example %>%
 detections.export <- detections_example %>%
   filter(fish_id %in% detection.sum$fish_id)
 
-use_data(detections_example, overwrite = TRUE)
+use_data(detections.export, overwrite = TRUE)
