2dca2ca0a33f3b11e9834ed7223a69ff8670fb36
Eli Felts <elifelts@gmail.com>
2025-12-30 13:24:49 -0700

    more example data tweaks

--- data-raw/fish_example_build.R
+++ data-raw/fish_example_build.R
@@ -5,17 +5,17 @@ library(usethis)
 
 detections <- read_feather("data-raw/detections_nov25")
 
-detections_example <- detections %>%
+detections_ex <- detections %>%
   filter(species == "WAE") %>%
   mutate(detection_year = year(detection_date)) %>%
   filter(detection_year == 2024)
 
-detection.sum <- detections_example %>%
+detection.sum <- detections_ex %>%
   group_by(fish_id) %>%
   summarize(locations = n_distinct(location_name)) %>%
   filter(locations > 15)
 
-detections.export <- detections_example %>%
+detections_example <- detections_ex %>%
   filter(fish_id %in% detection.sum$fish_id)
 
 use_data(detections.export, overwrite = TRUE)
