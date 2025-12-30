defff870f38b63a3386639378bec3a4e14585bcd
Eli Felts <elifelts@gmail.com>
2025-12-30 12:47:13 -0700

    example data added

---
+++ data-raw/fish_example_build.R
@@ -0,0 +1,21 @@
+library(arrow)
+library(tidyverse)
+library(usethis)
+
+
+detections <- read_feather("data-raw/detections_nov25")
+
+detections_example <- detections %>%
+  filter(species == "WAE") %>%
+  mutate(detection_year = year(detection_date)) %>%
+  filter(detection_year == 2024)
+
+detection.sum <- detections_example %>%
+  group_by(fish_id) %>%
+  summarize(locations = n_distinct(location_name)) %>%
+  filter(locations > 15)
+
+detections.export <- detections_example %>%
+  filter(fish_id %in% detection.sum$fish_id)
+
+use_data(detections_example, overwrite = TRUE)
