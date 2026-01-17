263fbbc41f058f9e7f219ff93c807f71068c8beb
Eli Felts <elifelts@gmail.com>
2026-01-17 11:13:02 -0800

    expand water area to include Clark Fork, Lightning, and Pack.

--- data-raw/build_paths2.R
+++ data-raw/build_paths2.R
@@ -6,6 +6,7 @@ library(terra)
 library(gdistance)
 library(leaflet)
 library(leafem)
+library(rnaturalearth)
 library(units)
 
 conflicts_prefer(
@@ -67,7 +68,7 @@ deployments.example <- receiver.dat |>
 # Farragut Breakwater
 
 p1 <- deployments.example |>
-  filter(location_name == "McDonald's Dock")
+  filter(location_name == "Pack Delta Buoys")
 
 p2 <- deployments.example |>
   filter(location_name == "Farragut Breakwater")
@@ -82,12 +83,59 @@ lake <- st_read(
   filter(name == "Lake Pend Oreille") |>
   st_transform(target_crs)
 
+# streams added too
+
+id.sf <- ne_states(returnclass = "sf") |>
+  filter(name == "Idaho")
+
+
+streams <- st_read(
+  dsn = "data-raw/idfg_habitat_mapping.gpkg",
+  layer = "streams"
+) |>
+  filter(name %in% c(
+    "Clark Fork",
+    "Lightning Creek",
+    "Pack River"
+  )) |>
+  st_intersection(id.sf) |>
+  st_transform(target_crs) |>
+  st_make_valid()
+
+# union the lake with a very small buffer of streams to force contact
+
+buf_m <- 30
+snap_buf_m <- 5
+
+lake_bridged <- st_union(lake, st_buffer(streams, snap_buf_m))
+lake_bridged <- st_make_valid(lake_bridged)
+
+stream_poly <- st_buffer(streams, buf_m) |> st_make_valid()
+
+water_surface <- st_union(lake_bridged, stream_poly) |>
+  st_make_valid() |>
+  st_cast("MULTIPOLYGON")
+
+lake_center <- st_point_on_surface(lake) |> st_geometry()
+
+parts <- st_cast(water_surface, "POLYGON")
+idx <- st_within(lake_center, parts, sparse = TRUE)[[1]]
+
+water_surface_main <- st_as_sf(parts[idx])
+
+clean_m <- 2
+water_surface_main <- water_surface_main |>
+  st_buffer(clean_m) |>
+  st_buffer(-clean_m) |>
+  st_make_valid()
+
+
 
 # Map that simple pair and lake polygon just to visualize
 # what's going on
 
 leaflet_base |>
-  addPolygons(data = st_transform(lake, crs = 4326)) |>
+  addPolygons(data = st_transform(water_surface_main, crs = 4326)) |>
   addCircleMarkers(
     data = st_transform(p1, 4326)
   ) |>
@@ -99,14 +147,14 @@ leaflet_base |>
 
 res_m <- 100
 
-r_template <- rast(ext(vect(lake)),
+r_template <- rast(ext(vect(water_surface_main)),
   resolution = res_m,
   crs = paste0("EPSG:", target_crs)
 )
 
 # rasterize: inside lake = 1, outside = NA
 
-lake_r <- rasterize(vect(lake), r_template, field = 1, background = NA)
+lake_r <- rasterize(vect(water_surface_main), r_template, field = 1, background = NA)
 
 
 # ---- 3) cost surface + transition graph ----
@@ -158,7 +206,7 @@ path_pts_sf <- st_as_sf(pts_sfc) |>
   st_cast("POINT")
 path_pts_sf$dist_m <- d
 
-st_crs(path_pts_sf) <- st_crs(lake)
+st_crs(path_pts_sf) <- st_crs(water_surface_main)
 
 path_pts_4326 <- st_transform(path_pts_sf, 4326)
 
@@ -177,7 +225,7 @@ plot(st_geometry(p1), add = TRUE, pch = 16)
 plot(st_geometry(p2), add = TRUE, pch = 16)
 
 leaflet_base |>
-  addPolygons(data = st_transform(lake, crs = 4326)) |>
+  addPolygons(data = st_transform(water_surface, crs = 4326)) |>
   addCircleMarkers(
     data = st_transform(p1, 4326)
   ) |>
@@ -187,4 +235,11 @@ leaflet_base |>
   addCircleMarkers(
     data = path_pts_4326,
     radius = 1
+  )
+
+leaflet_base |>
+  addPolygons(data = st_transform(water_surface_main, crs = 4326)) |>
+  addCircleMarkers(
+    data = st_transform(deployments.example, crs = 4326),
+    label = ~ str_c(location_name)
   )
